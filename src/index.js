dca3067d14e815c4ba71f52c6aa1b043914b34b3
Benjamin Chacko <benjaminchacko@outlook.com>
2019-08-15 11:50:47 -0500

    added counter & isLogged Reducers

--- src/index.js
+++ src/index.js
@@ -3,15 +3,47 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import createHistory from "history/createBrowserHistory";
 import "./css/index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import Home from './pages/home'
+import { createStore } from "redux";
+import allReducers from './reducers'
+
+import App from "./App";
+import Home from "./pages/home";
+
+
+
+// Store --> Globalize State
+
+// Action Increment
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  };
+};
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  };
+};
+
+// Reducer
+
+let store = createStore(
+    allReducers, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+//Display in Console
+store.subscribe(() => console.log(store.getState()));
+
+// Dispatch
+store.dispatch(decrement());
 
 ReactDOM.render(
   <Router history={createHistory()}>
     <App>
-    <Route path="/" exact component={Home} />
+      <Route path="/" exact component={Home} />
     </App>
   </Router>,
   document.getElementById("root")
